61bd5cecf57c9996801cebc8826cc97ec23622e5
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-02-21 12:54:21 +0100

    cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://kl0filinj.github.io/online-chat",
+    origin: "https://kl0filinj.github.io",
     methods: ["GET", "POST"],
   },
 });
